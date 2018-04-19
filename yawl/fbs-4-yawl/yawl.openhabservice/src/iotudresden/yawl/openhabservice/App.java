3d079f917c4feeb1a452b438ce11507ff2374a12
andiikaa <andre.kuehnie@gmail.com>
2018-04-19 11:15:46 +0200

    base for yawl oh

---
+++ yawl/fbs-4-yawl/yawl.openhabservice/src/iotudresden/yawl/openhabservice/App.java
@@ -0,0 +1,13 @@
+package iotudresden.yawl.openhabservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
